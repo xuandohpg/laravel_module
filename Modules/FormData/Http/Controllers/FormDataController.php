d9753ba3bbbf239f0dea9320d15778a22ac0beb2
Van Do <xuandohpg@gmail.com>
2022-04-24 22:20:45 +0700

    laptop 24/04/2022

--- Modules/FormData/Http/Controllers/FormDataController.php
+++ Modules/FormData/Http/Controllers/FormDataController.php
@@ -11,24 +11,6 @@ use Modules\FormData\Entities\FormData;
 class FormDataController extends Controller
 {
     function add(Request $request){
-
-        // return FormData::create([
-        //     'name' => $request->input('name'),
-        //     'tags' => $request->input('tags'),
-        //     'country' => $request->input('country'),
-        //     'approved_rate' => $request->input('approvedRate'),
-        //     'age_of_use' => $request->input('age'),
-        //     'price' => $request->input('price'),
-        //     'conversion_rate' => $request->input('conversionRate'),
-        //     'social_network' => $request->input('socialNetwork'),
-        //     'link_landing' => $request->input('linkLanding'),
-        //     'exp' => $request->input('exp'),
-        //     'payout' => $request->input('payout'),
-        //     'priority' => $request->input('priority'),
-        //     'image'=>$request->input('linkImage'),
-        //     'link_dinos'=>$request->input('linkDinos'),
-        // ]);
-
         // dd($request);
         return response()->json(FormData::create($request->all()), 201);
 
@@ -40,7 +22,7 @@ class FormDataController extends Controller
         $formdata->update($request->all());
         return response()->json($formdata, 200);
     }
-    // http://localhost:8000/api/formdata/update?email=dinhhoang@gmail.com&email_new=dinhhoang123@gmail.com&tags=health&country=vietnam&age=30&social_network=google&exp=1
+
 
 
     function index(){
